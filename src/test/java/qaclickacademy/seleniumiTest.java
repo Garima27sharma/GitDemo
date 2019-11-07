62eab1cc440b811ec482b80af28f87f2c0f30c4b
garima27sharma <garimasharma3281@gmail.com>
2019-11-07 22:57:36 +0530

    develope branch changes

--- src/test/java/qaclickacademy/seleniumiTest.java
+++ src/test/java/qaclickacademy/seleniumiTest.java
@@ -9,11 +9,13 @@ public class seleniumiTest
 	{
 		System.out.println("rundemo");
 		System.out.println("its selenium test run");
+		System.out.println("changes in develope branch");
 	}
 	@Test
 	public void defaultt()
 	{
 		System.out.println("default");
 		System.out.println("by defaault methods");
+		System.out.println("changes in develope branch by default");
 	}
 }
